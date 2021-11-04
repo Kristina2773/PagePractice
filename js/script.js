d718a98882c1919fd8e5b543a05657f8cc971c87
Kristina <d.kristina27@yandex.ru>
2021-11-04 18:51:59 +0400

    practice document

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
-'use strict';
+
 
 const movieDB = {
     movies: [
@@ -24,3 +24,28 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+    poster = document.querySelector(".promo__bg"),
+    genre = poster.querySelector('.promo__genre'),
+    movieList = document.querySelector('.promo__interactive-list');
+
+
+adv.forEach(item => {
+    item.remove();
+});
+
+genre.textContent = "Драма";
+
+poster.style.cssText = 'background-image: url(img/bg.jpg)';
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) =>{
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i+1}. ${film}
+        <div class="delete"></div>
+    </li>
+    `;
+});
